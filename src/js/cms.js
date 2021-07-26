8afcf86c75b2a86aca8ca3da8abe118193bdc0a4
Travis <travisryanbenton@gmail.com>
2021-07-26 16:56:04 -0500

    remove blog

--- src/js/cms.js
+++ src/js/cms.js
@@ -5,7 +5,6 @@ import CMS from "netlify-cms-app";
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
 import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/services";
 import PersonalStatements from "./cms-preview-templates/personal-statements";
 import OurProcess from "./cms-preview-templates/our-process";
@@ -16,7 +15,6 @@ import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("our-process", OurProcess);
 CMS.registerPreviewTemplate("personal-statements", PersonalStatements);

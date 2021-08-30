1395e3229a1819ed95cc54e46be35cc4aab7ff8f
Travis <travisryanbenton@gmail.com>
2021-08-30 11:34:07 -0500

    add copywriting services

--- site/content/services/_index.md
+++ site/content/services/_index.md
@@ -58,4 +58,32 @@ academic_services:
         - Simple Cover Letter Template
       plan: Premium Package
       price: "549"
+copywriting_services:
+  plans:
+    - items:
+        - Download of Job Document Best Practices
+        - Individual Job Tracker
+        - Simple Resume Template
+        - Simple Cover Letter Template
+      plan: Basic Guide
+      price: "49"
+    - items:
+        - 60 min. Resume and Cover Letter Consultation with Feedback
+        - Download of Job Document Best Practices
+        - Individual Job Tracker
+        - Simple Resume Template
+        - Simple Cover Letter Template
+      plan: Starter Package
+      price: "295"
+    - items:
+        - 60 min. Resume and Cover Letter Consultation with Feedback
+        - Revision and Second 60 min. Consultation with Feedback
+        - Interview Preparation
+        - LinkedIn Profile Review
+        - Download of Job Document Best Practices
+        - Individual Job Tracker
+        - Simple Resume Template
+        - Simple Cover Letter Template
+      plan: Premium Package
+      price: "549"
 ---
--- src/js/cms-preview-templates/services.js
+++ src/js/cms-preview-templates/services.js
@@ -14,6 +14,7 @@ export default class PostPreview extends React.Component {
           <select name="interest">
             <option>professional</option>
             <option>academic</option>
+            <option>copywriting</option>
           </select>
           services
         </h1>
@@ -51,6 +52,34 @@ export default class PostPreview extends React.Component {
 
             <div className="flex-ns mhn2-ns mw7" data-services="academic">
               {(entry.getIn(['data', 'academic_services', 'plans']) || []).map((plan, index) => <div className="w-33-ns ph2 mb5" key={index}>
+                <div className="ph2">
+
+                  <h3 className="b f5 grey-3 tc lh-title mb3">{plan.get('plan')}</h3>
+
+                  <p className="primary f1 b tc lh-title center">
+                    <span className="f4 v-mid">$</span>{plan.get('price')}
+                  </p>
+
+                	<p className="b">{plan.get('description')}</p>
+
+                  <ul>
+                    {(plan.get('items') || []).map((item, index) => <li key={index}>
+                      <p className={index + 1 !== plan.get('items').size ? "pb2 mb2 divider-grey" : null}>{item}</p>
+                    </li>)}
+                  </ul>
+
+                </div>
+
+              </div>)}
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-off-white mb3">
+          <div className="mw7 center">
+
+            <div className="flex-ns mhn2-ns mw7" data-services="academic">
+              {(entry.getIn(['data', 'copywriting_services', 'plans']) || []).map((plan, index) => <div className="w-33-ns ph2 mb5" key={index}>
                 <div className="ph2">
 
                   <h3 className="b f5 grey-3 tc lh-title mb3">{plan.get('plan')}</h3>

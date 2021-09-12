5e9163e27999f50f13dd5d729f98408b339bf60a
Travis <travisryanbenton@gmail.com>
2021-09-12 17:33:14 -0500

    contact bug pt 2

--- src/js/behavior/contact.js
+++ src/js/behavior/contact.js
@@ -1,9 +1,9 @@
 const [_, page] = window.location.pathname.split('/')
 if (page && page.toLowerCase() === 'contact') {
-  const service = new URLSearchParams(window.location.search).get('service')
+  const service = new URLSearchParams(window.location.search).get('service') || ''
   const pkg = new URLSearchParams(window.location.search).get('package')
   const messageInput = document.querySelector('[name=message]')
-  const template = (service = '', pkg) => {
+  const template = (service, pkg) => {
     if (service.toLowerCase() === 'copywriting') {
       return `Hi! I'm interested in learning more about your copywriting services.`
     }
